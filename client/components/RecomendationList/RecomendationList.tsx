426b9e06cae37eb69d9f28a9b255b3ecef3b62d3
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2025-12-19 09:08:14 +0530

    added number truncation and time truncation and image resize

--- client/components/RecomendationList/RecomendationList.tsx
+++ client/components/RecomendationList/RecomendationList.tsx
@@ -1,93 +1,17 @@
 import VideoCard from "@/components/VideoCard/VideoCard";
+import DummyData from "@/constants/DummyData";
 
 const RecomendationList = (): React.ReactElement => {
-  const data = [
-    {
-      id: "120",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "My Youtube channel",
-        logo: "/hq720.jpg",
-        id: "234",
-      },
-      title: "This is the recon tool you need in 2006",
-      createdAt: "12/12/32",
-      viewCount: 100,
-    },
-    {
-      id: "121",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Code Masters",
-        logo: "/hq720.jpg",
-        id: "234",
-      },
-      title: "Advanced Web Development Techniques",
-      createdAt: "12/12/32",
-      viewCount: 5240,
-    },
-    {
-      id: "122",
-      thumbnail: "/hq720.jpg",
-      channelLogo: "/hq720.jpg",
-      channel: {
-        name: "Dev Tutorials",
-        logo: "/hq720.jpg",
-        id: "235",
-      },
-      title: "React Performance Optimization Guide",
-      createdAt: "12/12/32",
-      viewCount: 12500,
-    },
-    {
-      id: "123",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Programming Insights",
-        logo: "/hq720.jpg",
-        id: "236",
-      },
-      title: "TypeScript Tips and Tricks",
-      createdAt: "12/12/32",
-      viewCount: 8340,
-    },
-    {
-      id: "124",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Tech Academy",
-        logo: "/hq720.jpg",
-        id: "237",
-      },
-      channelLogo: "/hq720.jpg",
-      title: "Full Stack Development with Next.js",
-      createdAt: "12/12/32",
-      viewCount: 15600,
-    },
-    {
-      id: "125",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Design Code",
-        logo: "/hq720.jpg",
-        id: "238",
-      },
-      title: "CSS Grid and Flexbox Mastery",
-      channelName: "Design Code",
-      createdAt: "12/12/32",
-      viewCount: 9870,
-    },
-  ];
   return (
     <div className="flex flex-col">
-      {data.map((videoData, idx) => {
+      {DummyData.map((videoData, idx) => {
         return (
           <VideoCard
             key={idx}
             thumbnailStyle={{
-              size: "8vw",
-              width: "100%",
-              height: "100%",
+              size: "80%",
+              width: "100px",
+              minWidth: "150px",
             }}
             videoInfoAlignment="horizontal"
             hideChannelLogo={true}
--- client/components/VideoList/VideoList.tsx
+++ client/components/VideoList/VideoList.tsx
@@ -1,86 +1,19 @@
 import VideoCard from "@/components/VideoCard/VideoCard";
+import DummyData from "@/constants/DummyData";
 const VideoList = (): React.ReactElement => {
-  const data = [
-    {
-      id: "120",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "My Youtube channel",
-        logo: "/hq720.jpg",
-        id: "234",
-      },
-      title: "This is the recon tool you need in 2006",
-      createdAt: "12/12/32",
-      viewCount: 100,
-    },
-    {
-      id: "121",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Code Masters",
-        logo: "/hq720.jpg",
-        id: "234",
-      },
-      title: "Advanced Web Development Techniques",
-      createdAt: "12/12/32",
-      viewCount: 5240,
-    },
-    {
-      id: "122",
-      thumbnail: "/hq720.jpg",
-      channelLogo: "/hq720.jpg",
-      channel: {
-        name: "Dev Tutorials",
-        logo: "/hq720.jpg",
-        id: "235",
-      },
-      title: "React Performance Optimization Guide",
-      createdAt: "12/12/32",
-      viewCount: 12500,
-    },
-    {
-      id: "123",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Programming Insights",
-        logo: "/hq720.jpg",
-        id: "236",
-      },
-      title: "TypeScript Tips and Tricks",
-      createdAt: "12/12/32",
-      viewCount: 8340,
-    },
-    {
-      id: "124",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Tech Academy",
-        logo: "/hq720.jpg",
-        id: "237",
-      },
-      channelLogo: "/hq720.jpg",
-      title: "Full Stack Development with Next.js",
-      createdAt: "12/12/32",
-      viewCount: 15600,
-    },
-    {
-      id: "125",
-      thumbnail: "/hq720.jpg",
-      channel: {
-        name: "Design Code",
-        logo: "/hq720.jpg",
-        id: "238",
-      },
-      title: "CSS Grid and Flexbox Mastery",
-      channelName: "Design Code",
-      createdAt: "12/12/32",
-      viewCount: 9870,
-    },
-  ];
   return (
-    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3  xl:grid-cols-3 2xl:grid-cols-6 gap-4">
-      {data.map((videoData, idx) => {
-        return <VideoCard key={idx} {...videoData} />;
+    <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {DummyData.map((videoData, idx) => {
+        return (
+          <VideoCard
+            key={idx}
+            {...videoData}
+            channelIconStyle={{
+              width: "3em",
+              aspectRatio: "1/1",
+            }}
+          />
+        );
       })}
     </div>
   );
