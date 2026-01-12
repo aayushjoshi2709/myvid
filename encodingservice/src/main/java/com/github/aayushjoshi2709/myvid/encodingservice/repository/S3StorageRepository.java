db7b926811be4e43f9a973124dffef0907048fc9
iamaayushjoshi <aayushjoshi2709@gmail.com>
2026-01-12 08:54:58 +0530

    added file downloader to encoding service

--- encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/repository/S3StorageRepository.java
+++ encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/repository/S3StorageRepository.java
@@ -1,25 +1,43 @@
 package com.github.aayushjoshi2709.myvid.encodingservice.repository;
 
+import java.nio.file.Paths;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
+
+import lombok.extern.slf4j.Slf4j;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.transfer.s3.S3TransferManager;
+import software.amazon.awssdk.transfer.s3.model.CompletedFileDownload;
+import software.amazon.awssdk.transfer.s3.model.DownloadFileRequest;
+import software.amazon.awssdk.transfer.s3.model.FileDownload;
 
 @Repository
+@Slf4j
 public class S3StorageRepository {
-        @Value("${aws.endpoint:#{null}}")
-        private String endpointUrl;
+        @Value("${aws.s3.data-processing.bucket-name}")
+        private String dataProcessingBucket;
+        private final S3TransferManager transferManager;
 
-        private final S3Presigner s3Presigner;
-
-        S3StorageRepository(S3Presigner s3Presigner) {
-                this.s3Presigner = s3Presigner;
+        public S3StorageRepository(
+                S3TransferManager transferManager
+        ){
+                this.transferManager = transferManager;
         }
 
         public void getFileFromS3(
-                String originalUrl, 
-                String downloadPath
-        ) {
+                        String key,
+                        String downloadedFileWithPath) {
+                DownloadFileRequest downloadFileRequest = DownloadFileRequest.builder()
+                                .getObjectRequest(b -> b.bucket(dataProcessingBucket).key(key))
+                                .destination(Paths.get(downloadedFileWithPath))
+                                .build();
+
+                FileDownload downloadFile = transferManager.downloadFile(downloadFileRequest);
+
+                CompletedFileDownload downloadResult = downloadFile.completionFuture().join();
+                log.info("Content length [{}]", downloadResult.response().contentLength());
         }
 
         public String saveFilesToS3(String originalUrl) {
--- encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/service/VideoEncodingService.java
+++ encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/service/VideoEncodingService.java
@@ -1,6 +1,5 @@
 package com.github.aayushjoshi2709.myvid.encodingservice.service;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.github.aayushjoshi2709.myvid.encodingservice.dto.PublishVideoDto;
 
 public interface VideoEncodingService {
