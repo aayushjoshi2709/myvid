2b5407fb10101861796e654013cb38eaede86702
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-01-30 00:46:40 +0530

    added analytics service

---
+++ analyticsservice/src/test/java/com/github/aayushjoshi2709/myvid/analyticsservice/AnalyticsserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aayushjoshi2709.myvid.analyticsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnalyticsserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
