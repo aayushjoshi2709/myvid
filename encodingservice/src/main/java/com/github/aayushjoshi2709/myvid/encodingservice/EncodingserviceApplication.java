4b39c59333c56b8f06965d8bfabba8fd2a0ade76
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-01-31 17:05:38 +0530

    revert move to server

--- encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/EncodingserviceApplication.java
+++ encodingservice/src/main/java/com/github/aayushjoshi2709/myvid/encodingservice/EncodingserviceApplication.java
@@ -7,9 +7,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @SpringBootApplication
 @EnableScheduling
 public class EncodingserviceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(EncodingserviceApplication.class, args);
 	}
-
 }
