75f7b4bc9b873ced5a236a80c46ab0e76774e6cd
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-05-25 14:56:22 +0530

    added services for comment gateway and auth

---
+++ server/commentservice/src/test/java/com/github/aayushjoshi2709/commentservice/CommentserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.aayushjoshi2709.commentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
