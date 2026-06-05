85e775143d3870502a72125c74392fb44c37da69
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-06-05 21:14:57 +0530

    connecting entities

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/controller/RoleController.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/controller/RoleController.java
@@ -1,5 +1,14 @@
 package com.github.aayushjoshi2709.authservice.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.aayushjoshi2709.authservice.service.RoleService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class RoleController {
-    
+    private final RoleService roleService;
+
 }
--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/controller/UserController.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/controller/UserController.java
@@ -1,5 +1,13 @@
 package com.github.aayushjoshi2709.authservice.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+import com.github.aayushjoshi2709.authservice.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
 public class UserController {
-    
+    private final UserService userService;
 }
