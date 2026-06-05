b29d30c5022551f45e0ccbfbc5b7359b5905d6b5
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-06-05 23:21:24 +0530

    added db schema

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/User.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/User.java
@@ -1,6 +1,8 @@
 package com.github.aayushjoshi2709.authservice.entity;
 
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSecondaryPartitionKey;
+
 import java.util.List;
 
 import com.github.aayushjoshi2709.authservice.entity.common.Common;
@@ -14,7 +16,13 @@ import lombok.Setter;
 @Builder
 public class User extends Common{
     private String name;
+    private String username;
     private String email;
     private String password;
     private List<String> roles;
+
+    @DynamoDbSecondaryPartitionKey(indexNames = "username-index")
+    public String getUsername() {
+        return username;
+    }
 }
