422c85d3a4218a8d00342803374bedcbb57216e0
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-06-05 21:09:21 +0530

    added dynamo db code for auth service

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/Role.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/Role.java
@@ -1,5 +1,17 @@
 package com.github.aayushjoshi2709.authservice.entity;
 
-public class Role {
-    
+import com.github.aayushjoshi2709.authservice.entity.common.Common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+
+@DynamoDbBean
+@Getter
+@Setter
+@Builder
+public class Role extends Common {
+    private String name;
+    private String description;
 }
--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/service/RoleService.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/service/RoleService.java
@@ -1,5 +1,11 @@
 package com.github.aayushjoshi2709.authservice.service;
 
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
 public class RoleService {
     
 }
