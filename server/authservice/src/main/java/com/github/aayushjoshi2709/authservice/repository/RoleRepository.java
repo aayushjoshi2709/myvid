61e60325e337431493e10d54746cb1c45c370347
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-06-05 23:21:24 +0530

    added db schema

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/RoleRepository.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/RoleRepository.java
@@ -2,7 +2,17 @@ package com.github.aayushjoshi2709.authservice.repository;
 
 import org.springframework.stereotype.Repository;
 
+import com.github.aayushjoshi2709.authservice.entity.Role;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
+
 @Repository
 public class RoleRepository {
-    
+    private final DynamoDbTable<Role> roleTable;
+
+    public RoleRepository(DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        this.roleTable = dynamoDbEnhancedClient.table("roles", TableSchema.fromBean(Role.class));
+    }
 }
--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/UserRepository.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/UserRepository.java
@@ -1,8 +1,30 @@
 package com.github.aayushjoshi2709.authservice.repository;
 
+import java.util.Optional;
+
 import org.springframework.stereotype.Repository;
+
+import com.github.aayushjoshi2709.authservice.entity.User;
+
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable;
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema;
 
 @Repository
 public class UserRepository {
+    private final DynamoDbTable<User> userTable;
+
+    public UserRepository(DynamoDbEnhancedClient dynamoDbEnhancedClient) {
+        this.userTable = dynamoDbEnhancedClient.table("users", TableSchema.fromBean(User.class));
+    }
+
+    public User save(User user) {
+        userTable.putItem(user);
+        return user;
+    }
+
+    public Optional<User> findById(String id) {
+        return Optional.ofNullable(userTable.getItem(r -> r.key(k -> k.partitionValue(id))));
+    }
     
 }
