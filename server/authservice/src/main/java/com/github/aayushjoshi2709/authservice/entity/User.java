279c7194a2d2cdde95229344534fa861716b4a31
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-06-05 21:09:21 +0530

    added dynamo db code for auth service

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/User.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/entity/User.java
@@ -1,5 +1,20 @@
 package com.github.aayushjoshi2709.authservice.entity;
 
-public class User {
-    
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import java.util.List;
+
+import com.github.aayushjoshi2709.authservice.entity.common.Common;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+@DynamoDbBean
+@Getter
+@Setter
+@Builder
+public class User extends Common{
+    private String name;
+    private String email;
+    private String password;
+    private List<String> roles;
 }
