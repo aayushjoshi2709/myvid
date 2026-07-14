0af1450eb28fb136e1bdcf206c9406f9b5ef0451
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-07-14 14:35:06 +0530

    added user repository and user service user related function

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/UserRepository.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.github.aayushjoshi2709.authservice.repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
+import com.github.aayushjoshi2709.authservice.entity.enums.UserStatusEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.github.aayushjoshi2709.authservice.entity.User;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, UUID> {}
+public interface UserRepository extends JpaRepository<User, UUID> {
+    Optional<User> findByIdAndStatus(UUID id, UserStatusEnum userStatusEnum);
+}
