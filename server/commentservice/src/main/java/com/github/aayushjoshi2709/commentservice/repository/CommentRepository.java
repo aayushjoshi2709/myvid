10c99ded0167e92b3bac88e8bffcfe850a412493
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-07-17 00:27:36 +0530

    update dir name

--- server/commentservice/src/main/java/com/github/aayushjoshi2709/commentservice/repository/CommentRepository.java
+++ server/commentservice/src/main/java/com/github/aayushjoshi2709/commentservice/repository/CommentRepository.java
@@ -1,9 +1,9 @@
 package com.github.aayushjoshi2709.commentservice.repository;
 
-import com.github.aayushjoshi2709.commentservice.entity.User;
+import com.github.aayushjoshi2709.commentservice.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface CommentRepository extends JpaRepository<User, UUID> {
+public interface CommentRepository extends JpaRepository<Comment, UUID> {
 }
