bb5b0d0915c9127bbf9d7de0e669e2c97a66701c
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-07-16 13:59:53 +0530

    moved jwt logic to separate jwt service

--- server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/RefreshTokenRepository.java
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/repository/RefreshTokenRepository.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
@@ -19,7 +20,7 @@ public interface RefreshTokenRepository extends JpaRepository<RefreshToken, UUID
     @Query("DELETE FROM RefreshToken rt WHERE rt.createdAt <= :tokenExpiryTime")
     void deleteExpiredTokens(@Param("tokenExpiryTime") LocalDateTime tokenExpiryTime);
 
-    RefreshToken findByRefreshToken(String refreshToken);
+    Optional<RefreshToken> findByRefreshToken(String refreshToken);
 
     List<RefreshToken> findByUserId(UUID userId);
 }
---
+++ server/authservice/src/main/java/com/github/aayushjoshi2709/authservice/service/JwtService.java
@@ -0,0 +1,7 @@
+package com.github.aayushjoshi2709.authservice.service;
+
+import com.github.aayushjoshi2709.authservice.entity.User;
+
+public interface JwtService {
+    String generateJWTToken(User user);
+}
