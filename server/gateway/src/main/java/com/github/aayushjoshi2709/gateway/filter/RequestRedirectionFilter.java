19f96f4fdb7402d1f51f64de8f7fdaa1803d7bd2
aayushjoshi2709 <aayushjoshi2709@gmail.com>
2026-07-17 10:52:49 +0530

    added request id and skipped other filters for nwo also added config for r2dbc going to install postgres driver in next commit maybe because i am stupid

--- server/gateway/src/main/java/com/github/aayushjoshi2709/gateway/filter/RequestRedirectionFilter.java
+++ server/gateway/src/main/java/com/github/aayushjoshi2709/gateway/filter/RequestRedirectionFilter.java
@@ -1,14 +1,18 @@
 package com.github.aayushjoshi2709.gateway.filter;
 
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.WebFilter;
 import org.springframework.web.server.WebFilterChain;
 
 import reactor.core.publisher.Mono;
 
+@Component
+@Order(5)
 class RequestRedirectionFilter implements WebFilter {
   @Override
   public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
-    return null;
+    return chain.filter(exchange);
   }
 }
